dbe6c869b6a09558d2e4b80b266df392b44a6eba
nilseling <nils.eling@gmx.de>
2019-09-17 18:31:58 +0200

    Reading in files

--- R/readCPout.R
+++ R/readCPout.R
@@ -4,9 +4,15 @@
 #' @param cells Path to file containing the features extracted per cell.
 #' Rows are cells and columns are features. 
 #' This file should contain columns labeled as 'MeanIntensity' which contain the mean ion counts per cell and channel.
+#' This should be a .csv file.
 #' @param image Path to file containin the image metadata.
+#' This should be a .csv file.
 #' @param relationships Path to file containing the spatial relationships between cells.
+#' This should be a tab-separated file.
 #' @param panel Path to file containing the spatial relationships between cells.
+#' This should be a .csv file.
+#' @param mean_channel Character that indicates which columns contain the mean ion counts per cell.
+#' All other columns will be stored in the \code{rowData} slot of the SingleCellExperiment.
 #' @param scale_counts Logical indicating whether the counts should be scaled by the image encoding factor.
 #' For example, Cell Profiler scales 16-bit images down by a factor of 2^16.
 #' To obtain the original counts, the MeanIntensity values need to be rescaled.   
@@ -15,20 +21,40 @@
 #' Cells are columns and rows are channels
 #' 
 #' @examples
-#' data(PBMC_fs, PBMC_panel, PBMC_md)
-#' prepData(PBMC_fs, PBMC_panel, PBMC_md)
+#' readCPout(system.file("extdata", "cells.csv", package = "imcRtools"),
+#'           system.file("extdata", "image.csv", package = "imcRtools"),
+#'           system.file("extdata", "Object_relationshipts.txt", package = "imcRtools"),
+#'           system.file("extdata", "panel.csv", package = "imcRtools"))
 #' 
-#' @importFrom methods new
-#' @importFrom dplyr mutate_all rename
-#' @importFrom flowCore colnames exprs exprs<- flowSet 
-#'   fsApply identifier isFCSfile keyword read.flowSet
+#' @author Nils Eling \email{nils.eling@@uzh.ch}
+#' 
 #' @importFrom SingleCellExperiment SingleCellExperiment
-#' @importFrom S4Vectors DataFrame
 #' @export
-
 
 readCPout <- function(cells,
                       image_meta,
                       relationships, 
                       panel,
-                      scale_counts = TRUE)
+                      mean_channels = "MeanIntensity_FullStackFiltered",
+                      scale_counts = TRUE){
+  
+  # Initial checks if files exist and are provided in the right format
+  errors <- .fileChecks(cells, image_meta, relationships, panel)
+  if(length(errors) > 0){stop(errors)}
+  
+  # Read in the files
+  tryCatch(cells.mat <- read.csv(cells), 
+           error = function(e){"The file containing the cell features could not be read in.\n
+             Please check the file format."})
+  tryCatch(image.mat <- read.csv(image_meta), 
+           error = function(e){"The file containing the image metadata could not be read in.\n
+             Please check the file format."})
+  tryCatch(relationships.mat <- read.delim(relationships), 
+           error = function(e){"The file containing the cell-cell relationships could not be read in.\n
+             Please check the file format."})
+  tryCatch(panel.mat <- read.csv(panel), 
+           error = function(e){"The file containing the cell-cell relationships could not be read in.\n
+             Please check the file format."})
+  
+  errors <- .inputChecks()
+}
