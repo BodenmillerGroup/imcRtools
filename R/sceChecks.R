4bcc2d1a3ca17f7035e84ccbccb45772dbaf3d36
nilseling <nils.eling@gmx.de>
2019-11-28 11:20:59 +0100

    Script to check SCE object

---
+++ R/sceChecks.R
@@ -0,0 +1,13 @@
+# Helper functions to check entries and validity of SingleCellExperiment
+.sceCheck <- function(x){
+  if(!is(x, "SingleCellExperiment")){
+    stop("x is not a SingleCellExperiment object.")
+  }
+}
+
+.assayCheck <- function(x, exprs_values){
+  .sceCheck(x)
+  if(!(exprs_values %in% names(assays(x)))){
+    stop(paste("The", exprs_values, "slot does not exists in the SingleCellExperiment object."))
+  }
+}
