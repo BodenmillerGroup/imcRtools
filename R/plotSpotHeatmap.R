780316ef6146d6f33271e588ab61ac82ed2facc5
nilseling <nils.eling@gmx.de>
2021-05-16 19:03:41 +0200

    Small adjustments for flexibility purposes

--- R/plotSpotHeatmap.R
+++ R/plotSpotHeatmap.R
@@ -1,8 +1,8 @@
-#' @title Visualizes the median pixel intensities per spot and channel
+#' @title Summarizes and visualizes the pixel intensities per spot and channel
 #'
 #' @description Helper function for estimating the spillover matrix. This
 #' function visualizes the median pixel intensities per spot (rows) and per
-#' channel (columns).
+#' channel (columns) in form of a heatmap.
 #'
 #' @param object a \code{SingleCellExperiment} object containing pixel
 #' intensities per channel. Individual pixels are stored as columns and
@@ -12,16 +12,55 @@
 #' form (mt)(mass) (e.g. Sm152 for Samarium isotope with the atomic mass 152).
 #' @param channel_id character string indicating which \code{rowData(object)} 
 #' entry contains the isotope names of the acquired channels. 
-#' @param assay_type character string indicating which assay to use.
+#' @param assay_type character string indicating which assay to use (default
+#' \code{counts}).
 #' @param statistic the statistic to use when aggregating channels per spot
+#' (default \code{median})
 #' @param log should the aggregated pixel intensities be \code{log10(x + 1)} 
 #' transformed?
-#' @param threshold single numeric indicating the aggregated pixel threshold.
-#' This facilitates the identification of spots with low aggregated intensities. 
+#' @param threshold single numeric indicating a threshold after pixel
+#' aggregation. All aggregated values larger than \code{threshold} will be
+#' labeled as \code{1}.
 #' @param order_metals should the metals be ordered based on spotted mass?
-#' @param ... arguments passed to \code{pheatmap}.
+#' \describe{
+#' \item{\code{color}, \code{breaks}, \code{legend_breaks}, \code{cluster_cols},
+#' \code{cluster_rows}}{parameters as defined in \code{\link[pheatmap]{pheatmap}}} 
+#' }
+#' @param ... other arguments passed to \code{pheatmap}.
+#' 
+#' @section Quality control for spillover estimation:
+#' Visualizing the aggregated pixel intensities serves two purposes:
+#' 
+#' \enumerate{
+#' \item Small median pixel intensities (< 200 counts) might hinder the robust
+#' estimation of the channel spillover. In that case, consecutive pixels can be
+#' summed (see \code{\link{binAcrossPixels}}).
+#' \item Each spotted metal (row) should show the highest median pixel intensity
+#' in its corresponding channel (column). If this is not the case, either the
+#' naming of the .txt files was incorrect or the incorrect metal was spotted.
+#' }
+#' 
+#' By setting the \code{threshold} parameter, the user can easily identify spots
+#' where pixel intensities are too low for robust spillover estimation. 
+#' 
+#' @return a \code{\link[pheatmap]{pheatmap}} object
 #'
 #' @examples
+#' path <- system.file("extdata/spillover", package = "imcRtools")
+#' Read in .txt
+#' sce <- readSCEfromTXT(path)
+#' 
+#' # Visualizes heatmap
+#' plotSpotHeatmap(sce)
+#' 
+#' # Visualizes thresholding results
+#' plotSpotHeatmap(sce, log = FALSE, threshold = 200)
+#' 
+#' @seealso \code{\link[pheatmap]{pheatmap}} for visual modifications
+#' @seealso \code{\link[scuttle]{aggregateAcrossCells}} for the aggregation
+#' function
+#' 
+#' @author Nils Eling (\email{nils.eling@@dqbm.uzh.ch})
 #'
 #' @importFrom pheatmap pheatmap
 #' @importFrom scuttle aggregateAcrossCells
@@ -33,14 +72,21 @@ plotSpotHeatmap <- function(object,
                             spot_id = "sample_id",
                             channel_id = "channel_name",
                             assay_type = "counts",
-                            statistic = "median",
+                            statistic = c("median", "mean", "sum"),
                             log = TRUE,
                             threshold = NULL,
                             order_metals = TRUE,
+                            color = viridis(100),
+                            breaks = NA,
+                            legend_breaks = NA,
+                            cluster_cols = FALSE,
+                            cluster_rows = FALSE,
                             ...){
     
     .valid.plotSpotHeatmap.input(object, spot_id, channel_id, assay_type, 
                            log, threshold, order_metals)
+    
+    statistic <- match.arg(statistic)
     
     cur_out <- aggregateAcrossCells(object, object[[spot_id]], 
                                     statistics = statistic,
@@ -54,19 +100,20 @@ plotSpotHeatmap <- function(object,
     
     if (!is.null(threshold)) {
         cur_mat <- (cur_mat > threshold) * 1
+        
+        if (is.na(breaks)) { 
+            breaks <- c(0, 0.5, 1)
+        }
+        
+        if (is.na(legend_breaks)) { 
+            legend_breaks <- c(0, 1)
+        }
+
+        color <- c(color[1], color[length(color)])
     }
     
     colnames(cur_mat) <- cur_out[[spot_id]] 
     rownames(cur_mat) <- rowData(cur_out)[[channel_id]] 
-    
-    # Overwrite default colour
-    args <- list(...)
-    
-    if ("color" %in% names(args)) {
-        cur_col <- args$color 
-    } else {
-        cur_col <- viridis(100)
-    }
     
     # Order rows and cols based on spot metal
     if (order_metals) {
@@ -83,15 +130,15 @@ plotSpotHeatmap <- function(object,
         
         cur_mat <- cur_mat[cur_rownames,cur_spots]
         
-        cur_cluster_cols <- FALSE
-        cur_cluster_rows <- FALSE
-    } else {
-        cur_cluster_cols <- ifelse("cluster_cols" %in% names(args), args$cluster_cols, TRUE)
-        cur_cluster_rows <- ifelse("cluster_rows" %in% names(args), args$cluster_rows, TRUE)
+        cluster_cols <- FALSE
+        cluster_rows <- FALSE
     }
     
     # Transposed to match the CATALYST visualization
-    pheatmap(t(cur_mat), color = cur_col, 
-             cluster_cols = cur_cluster_cols,
-             cluster_rows = cur_cluster_rows)
+    pheatmap(t(cur_mat), color = color, 
+            cluster_cols = cluster_cols,
+            cluster_rows = cluster_rows, 
+            breaks = breaks, legend_breaks = legend_breaks,
+            ...)
+    
 }
