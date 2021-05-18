70ce14fe507827689c95b4f6e444b887d923aa70
nilseling <nils.eling@gmx.de>
2021-05-18 15:24:54 +0200

    Added readImagefromTXT function

--- R/readImagefromTXT.R
+++ R/readImagefromTXT.R
@@ -27,15 +27,39 @@
 #' @author Nils Eling (\email{nils.eling@@dqbm.uzh.ch})
 #' 
 #' @importFrom cytomapper CytoImageList channelNames
+#' @importFrom abind abind
+#' @importFrom EBImage Image
+#' @importFrom stringr str_extract
+#' @importFrom readr read_delim
 #' @export
 readSCEfromTXT <- function(path, 
                            pattern = ".txt$",
                            channel_pattern = "[A-Za-z]{1,2}[0-9]{2,3}Di",
+                           index_names = c("X", "Y", "Z"),
                            verbose = TRUE){
     
+    cur_files <- list.files(path, pattern = pattern, full.names = TRUE)
     
+    cur_dat <- suppressMessages(lapply(cur_files, read_delim, delim = "\t"))
+    cur_dat <- lapply(cur_dat, function(x){
+        cur_mat <- x[,grepl(channel_pattern, colnames(x))]
+        cur_ind <- x[,index_names]
+        
+        n_row <- max(cur_ind[,index_names[1]]) + 1
+        n_col <- max(cur_ind[,index_names[2]]) + 1
+        
+        cur_mat <- lapply(cur_mat, function(y){
+            matrix(y, nrow = n_row, ncol = n_col, byrow = FALSE)
+        })
+        
+        cur_mat <- do.call(abind, list(cur_mat, along = 3))
+        
+        Image(cur_mat)
+    })
     
+    cur_dat <- CytoImageList(cur_dat)
     
-    return(cil)
-       
+    channelNames(cur_dat) <- str_extract(channelNames(cur_dat), channel_pattern)
+    
+    return(cur_dat)
 }
---
+++ tests/testthat/test_readImagefromTXT.R
@@ -0,0 +1,5 @@
+test_that("readImagefromTXT function works.", {
+    path <- system.file("extdata/mockData", package = "imcRtools")
+    
+    
+})
