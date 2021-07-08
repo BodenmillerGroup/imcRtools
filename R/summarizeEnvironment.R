46fdf746e02b700fc8e83bde7e6b29c24500711f
SchulzDan <shooltz@gmx.de>
2021-07-08 14:26:27 +0000

    adapted function

--- R/summarizeEnvironment.R
+++ R/summarizeEnvironment.R
@@ -1,10 +1,32 @@
-# function to generate a data frame with the count of cells of a specified type (e.g. clustered celltypes) in the neighborhood of each cell.
-# ... to be written
+#' @title Generates a data frame with the count of cells of a specified type (e.g. clustered celltypes) in the selected neighborhood of each cell.
+#'
+#' @description Function to generates a data frame with the count of cells of a specified type (e.g. clustered celltypes)
+#' in the selected neighborhood of each cell.
+#'
+#' @param object a \code{SingleCellExperiment} object
+#' @param colPairName single character indicating the \code{colPair(object)} entry
+#' containing the the neighbor information.
+#' @param group single character specifying the \code{colData(object)} entry containing the
+#' cellular features that should be summarized in the neighborhood.
+#' Supported entries are of type character.
+#' @param name single character specifying the name of the data frame.
+#'
+#' @return returns a \code{SingleCellExperiment}
+#' containing the data frame in form of a \code{DataFrame} object in
+#' \code{colData(object)}.
+#'
+#'
+#' @author Daniel Schulz (\email{daniel.schulz@@uzh.ch})
+#'
+#' @importFrom dplyr as_tibble group_by count select
+#' @importFrom tidyr pivot_wider
+#' @importFrom S4Vectors DataFrame
+#' @export
 
-summarizeEnvironment <- function(object,
-                                 graphName,
-                                 group,
-                                 name = NULL){
+summarizeNeighbors <- function(object,
+                               colPairName,
+                               group,
+                               name = NULL){
 
   if (! graphName %in% colPairNames(object)) {
     stop("graphName is not a graph in the current object")
@@ -14,12 +36,10 @@ summarizeEnvironment <- function(object,
     stop("group is not a valid colData entry in the current object")
   }
 
-  cur_graph <- graph_from_edgelist(as.matrix(colPair(object,graphName)))
-
-  cur_dat <- as_data_frame(cur_graph)
+  cur_dat <- as_tibble(colPair(object,graphName))
 
   cur_out <- cur_dat %>%
-    mutate(celltype =   colData(object)[[group]][cur_dat$to]) %>%
+    mutate(celltype = colData(object)[[group]][cur_dat$to]) %>%
     group_by(from,.drop=FALSE) %>%
     count(celltype) %>%
     pivot_wider(names_from = celltype,values_from = n, values_fill = 0) %>%
@@ -28,9 +48,9 @@ summarizeEnvironment <- function(object,
 
   # add the dataframe to the object
 
-  name <- ifelse(is.null(name), "summarizedEnvironment", name)
+  name <- ifelse(is.null(name), "summarizedNeighbors", name)
 
-  colData(object)[[name]] <- cur_out
+  colData(object)[[name]] <- DataFrame(cur_out)
 
   return(object)
 
