9ec8f78b7fade748cac6630ef2ce450af90b5f8b
nilseling <nils.eling@gmx.de>
2021-07-26 12:57:41 +0200

    Expanded unit tests on knn

--- tests/testthat/test_aggregateNeighbors.R
+++ tests/testthat/test_aggregateNeighbors.R
@@ -13,13 +13,82 @@ test_that("aggregateNeighbors function works", {
                                      name = "exp_20")
     
     # Works metadata version on knn
+    # Directed
+    expect_silent(cur_sce <- aggregateNeighbors(object = pancreasSCE,
+                                                colPairName = "knn_10",
+                                                aggregate_by = "metadata",
+                                                count_by = "CellType"))
+    
+    expect_s4_class(cur_sce , class = "SingleCellExperiment")
+    expect_equal(names(colData(cur_sce)), 
+                 c("ImageName", "Pos_X", "Pos_Y", "Area", "CellType", "ImageNb",
+                   "CellNb", "MaskName", "Pattern", "aggregatedNeighbors"))
+    expect_s4_class(cur_sce$aggregatedNeighbors, "DataFrame")
+    expect_true(all(rowSums(as.matrix(cur_sce$aggregatedNeighbors)) == 1))
+    
+    # check for correct results of neighboring metadata
+    cur_dat <- data.frame(colPair(pancreasSCE,"knn_10"))
+    cur_dat$celltype <- factor(colData(cur_sce)$CellType[cur_dat$to])
+    
+    cur_dat <- cur_dat %>% group_by(from) %>% count(celltype, .drop = FALSE) %>% 
+        pivot_wider(names_from = "celltype", values_from = "n") %>% ungroup() %>% 
+        select(-from) %>% as.matrix()
+    
+    cur_dat <- cur_dat / rowSums(cur_dat)
+    
+    expect_equal(cur_sce$aggregatedNeighbors,
+                 DataFrame(cur_dat))
+    
     expect_silent(cur_sce <- aggregateNeighbors(object = pancreasSCE,
                                                 colPairName = "knn_10",
                                                 aggregate_by = "metadata",
                                                 count_by = "CellType",
-                                                name = "knn_10_nb"))
+                                                proportions = FALSE))
     
     expect_s4_class(cur_sce , class = "SingleCellExperiment")
+    expect_equal(names(colData(cur_sce)), 
+                 c("ImageName", "Pos_X", "Pos_Y", "Area", "CellType", "ImageNb",
+                   "CellNb", "MaskName", "Pattern", "aggregatedNeighbors"))
+    expect_s4_class(cur_sce$aggregatedNeighbors, "DataFrame")
+    expect_true(all(rowSums(as.matrix(cur_sce$aggregatedNeighbors)) == 10))
+    
+    # check for correct results of neighboring metadata
+    cur_dat <- data.frame(colPair(pancreasSCE,"knn_10"))
+    cur_dat$celltype <- factor(colData(cur_sce)$CellType[cur_dat$to])
+    
+    cur_dat <- cur_dat %>% group_by(from) %>% count(celltype, .drop = FALSE) %>% 
+        pivot_wider(names_from = "celltype", values_from = "n") %>% ungroup() %>% 
+        select(-from) %>% as.matrix()
+    
+    expect_equal(cur_sce$aggregatedNeighbors,
+                 DataFrame(cur_dat))
+    
+    cur_sce2 <- cur_sce
+    colPair(cur_sce2, "knn_10") <- colPair(cur_sce2, "knn_10")[from(colPair(cur_sce2, "knn_10")) == 215,]
+    cur_sce2 <- cur_sce2[,c(215, to(colPair(cur_sce2, "knn_10"))[from(colPair(cur_sce2, "knn_10")) == 215])]
+    
+    plotSpatial(cur_sce2, img_id = "ImageNb", node_color_by = "CellType", draw_edges = TRUE,
+                colPairName = "knn_10")
+    cur_sce2$aggregatedNeighbors
+    
+    cur_sce2 <- cur_sce
+    colPair(cur_sce2, "knn_10") <- colPair(cur_sce2, "knn_10")[from(colPair(cur_sce2, "knn_10")) == 10,]
+    cur_sce2 <- cur_sce2[,c(10, to(colPair(cur_sce2, "knn_10"))[from(colPair(cur_sce2, "knn_10")) == 10])]
+    
+    plotSpatial(cur_sce2, img_id = "ImageNb", node_color_by = "CellType", draw_edges = TRUE,
+                colPairName = "knn_10")
+    cur_sce2$aggregatedNeighbors
+    
+    cur_sce2 <- cur_sce
+    colPair(cur_sce2, "knn_10") <- colPair(cur_sce2, "knn_10")[from(colPair(cur_sce2, "knn_10")) == 322,]
+    cur_sce2 <- cur_sce2[,c(322, to(colPair(cur_sce2, "knn_10"))[from(colPair(cur_sce2, "knn_10")) == 322])]
+    
+    plotSpatial(cur_sce2, img_id = "ImageNb", node_color_by = "CellType", draw_edges = TRUE,
+                colPairName = "knn_10")
+    cur_sce2$aggregatedNeighbors
+    
+    # Undirected
+    
     
     # Works expression version on knn
     cur_markers <- rownames(pancreasSCE)
