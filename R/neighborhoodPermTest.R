f562ae103b870bba818d25666db712d94135ffb1
nilseling <nils.eling@gmx.de>
2021-08-05 19:58:16 +0200

    Docs on summarizeNeighbors function

--- R/neighborhoodPermTest.R
+++ R/neighborhoodPermTest.R
@@ -1,22 +1,85 @@
 #' @rdname neighborhoodPermTest
-#' @title Performs perturbations to test if two cell-types interact more or less frequently than random.
+#' @title Performs perturbations to test if two cell-types interact more or 
+#' less frequently than random.
 #'
-#' @description This function computes summary statistics (mean, median, and variance) within each factor level.
-#'  When multiple entries are provided, summary statistics are calculated for eachcombination of factor entries.
+#' @description Cell-cell interactions are summarized in different ways and
+#' the resulting count is compared to a distribution of counts arising 
+#' from random permutations.
 #'
-#' @param object a \code{\link[SingleCellExperiment]{SingleCellExperiment}}.
+#' @param object a \code{SingleCellExperiment} or \code{SpatialExperiment}
+#' object .
+#' @param group_by a single character indicating the \code{colData(object)}
+#' entry by which interactions are grouped. This is usually the image ID or
+#' patient ID.
+#' @param label single character specifying the \code{colData(object)} entry
+#' which stores the cell labels. These can be cell-types labels or other m
+#' metadata. 
+#' @param method which cell-cell interaction counting method to use 
+#' (see details)
+#' @param patch_size if \code{method = "patch"}, a single numeric specifying
+#' the minimum number of neighbors of the same type to be considered a patch
+#' (see details)
+#' @param colPairName single character indicating the \code{colPair(object)}
+#' entry containing cell-cell interactions in form of an edge list.
+#' @param iter single numeric specifying the number of permutations to perform
+#' @param p_threshold single numeric indicating the empirical p-value 
+#' threshold at which interactions are considered to be significantly 
+#' enriched or depleted per group.
+#' @param BBPARAM parameters for parallelized processing. 
 #'
-#' @section Counting interactions
+#' @section Counting and summarizing cell-cell interactions
 #' 
-#' Explain the three different approaches
+#' In principle, the \code{summarizeNeighborhood} function counts the number
+#' of edges (interactions) between each set of unique entries in 
+#' \code{colData(object)[[label]]}. Simplified, it counts for each cell of
+#' type A the number of neighbors of type B.
+#' 
+#' This count can be averaged within each unique entry 
+#' \code{colData(object)[[group_by]]} in three different ways:
+#' 
+#' 1. \code{method = "classic"}: The count is divided by the total number of 
+#' cells of type A. The final count can be interpreted as "How many neighbors 
+#' of type B does a cell of type A have on average?" 
+#' 
+#' 2. \code{method = "classic"}: The count is divided by the number of cells
+#' of type A that have at least one neigbor of type B. The final count can be 
+#' interpreted as "How many many neightbours of type B has a cell of type A on 
+#' average, given it has at least one neigthbour of type B?"
+#' 
+#' 3. \code{method = "patch"}: For each cell, the count is binarized to 0 
+#' (less than \code{patch_size} neighbors of type B) or 1 (more or equal to 
+#' \code{patch_size} neighbors of type B). The binarized counts are averaged 
+#' across all cells of type A. The final count can be interpreted as "What 
+#' fraction of cells of type A have at least a given number of neighbors of 
+#' type B?"
+#' 
+#' @section Testing for significance
+#' 
+#' 
 #'  
 #' @examples 
 #' # TODO
+#' 
+#' @seealso 
+#' \code{\link{summarizeNeighborhood}} for counting (but not testing) cell-cell
+#' interactions per grouping level.
+#'   
+#' \code{\link[BiocParallel]{bpparam}} for the parallelised backend
 #'
 #' @author Vito Zanotelli
 #' @author Jana Fischer
 #' @author adapted by Nils Eling (\email{nils.eling@@uzh.ch})
-#'
+#' 
+#' @references
+#' \href{https://www.sciencedirect.com/science/article/pii/S2405471217305434?via%3Dihub}{
+#' Schulz, D. et al., Simultaneous Multiplexed Imaging of mRNA and Proteins with 
+#' Subcellular Resolution in Breast Cancer Tissue Samples by Mass Cytometry., 
+#' Cell Systems 2018 6(1):25-36.e5}
+#' 
+#' \href{https://www.nature.com/articles/nmeth.4391}{
+#' Shapiro, D. et al., histoCAT: analysis of cell phenotypes and interactions in 
+#' multiplex image cytometry data, Nature Methods 2017 14, p. 873–876}
+#' 
 #' @export
 neighborhoodPermTest <- function(object, 
                                  group_by,
--- R/summarizeNeighborhood.R
+++ R/summarizeNeighborhood.R
@@ -1,13 +1,50 @@
 #' @rdname summarizeNeighborhood
-#' @title Summarizes cell-cell interactions 
+#' @title Summarizes cell-cell interactions within grouping levels (e.g. images)
 #'
-#' @description TODO
+#' @description Function to calculate the average number of neighbors B that
+#' a cell of type A has using different approaches. 
 #'
-#' @param object a \code{\link[SingleCellExperiment]{SingleCellExperiment}}.
+#' @param object a \code{SingleCellExperiment} or \code{SpatialExperiment}
+#' object .
+#' @param group_by a single character indicating the \code{colData(object)}
+#' entry by which interactions are grouped. This is usually the image ID or
+#' patient ID.
+#' @param label single character specifying the \code{colData(object)} entry
+#' which stores the cell labels. These can be cell-types labels or other m
+#' metadata. 
+#' @param method which cell-cell interaction counting method to use 
+#' (see details)
+#' @param patch_size if \code{method = "patch"}, a single numeric specifying
+#' the minimum number of neighbors of the same type to be considered a patch
+#' (see details)
+#' @param colPairName single character indicating the \code{colPair(object)}
+#' entry containing cell-cell interactions in form of an edge list.
 #'
-#' @section Counting interactions
+#' @section Counting and summarizing cell-cell interactions
 #' 
-#' Explain the three different approaches
+#' In principle, the \code{summarizeNeighborhood} function counts the number
+#' of edges (interactions) between each set of unique entries in 
+#' \code{colData(object)[[label]]}. Simplified, it counts for each cell of
+#' type A the number of neighbors of type B.
+#' 
+#' This count can be averaged within each unique entry 
+#' \code{colData(object)[[group_by]]} in three different ways:
+#' 
+#' 1. \code{method = "classic"}: The count is divided by the total number of 
+#' cells of type A. The final count can be interpreted as "How many neighbors 
+#' of type B does a cell of type A have on average?" 
+#' 
+#' 2. \code{method = "classic"}: The count is divided by the number of cells
+#' of type A that have at least one neigbor of type B. The final count can be 
+#' interpreted as "How many many neightbours of type B has a cell of type A on 
+#' average, given it has at least one neigthbour of type B?"
+#' 
+#' 3. \code{method = "patch"}: For each cell, the count is binarized to 0 
+#' (less than \code{patch_size} neighbors of type B) or 1 (more or equal to 
+#' \code{patch_size} neighbors of type B). The binarized counts are averaged 
+#' across all cells of type A. The final count can be interpreted as "What 
+#' fraction of cells of type A have at least a given number of neighbors of 
+#' type B?"
 #'  
 #' @examples 
 #' # TODO
@@ -15,6 +52,16 @@
 #' @author Vito Zanotelli
 #' @author Jana Fischer
 #' @author adapted by Nils Eling (\email{nils.eling@@uzh.ch})
+#' 
+#' @references
+#' \href{https://www.sciencedirect.com/science/article/pii/S2405471217305434?via%3Dihub}{
+#' Schulz, D. et al., Simultaneous Multiplexed Imaging of mRNA and Proteins with 
+#' Subcellular Resolution in Breast Cancer Tissue Samples by Mass Cytometry., 
+#' Cell Systems 2018 6(1):25-36.e5}
+#' 
+#' \href{https://www.nature.com/articles/nmeth.4391}{
+#' Shapiro, D. et al., histoCAT: analysis of cell phenotypes and interactions in 
+#' multiplex image cytometry data, Nature Methods 2017 14, p. 873–876}
 #'
 #' @export
 summarizeNeighborhood <- function(object, 
