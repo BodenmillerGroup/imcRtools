033dc744384e93b4b24d0813cd38f79aea684e48
nilseling <nils.eling@gmx.de>
2021-10-08 13:31:19 +0200

    Added pipe-friendly polygon construction

--- R/patchDetection_helpers.R
+++ R/patchDetection_helpers.R
@@ -22,7 +22,34 @@
             cells <- st_multipoint(as.matrix(cur_coords))
             cells_sfc = st_cast(st_sfc(cells), "POINT")
             
+            cur_out <- colData(cur_obj) %>% as_tibble %>%
+                filter(!is.na(!!sym(name))) %>% 
+                nest_by(!!sym(name)) %>%
+                summarize(polygon = list(hull_function(x = data, 
+                                                  coords = coords, 
+                                                  convex = convex)))
+            
             
         }, BPPARAM = BPPARAM)
+    
+}
+
+hull_function <- function(x, coords, convex){
+    
+    if (convex) {
+        hull <- chull(x = x[[coords[1]]], y = x[[coords[2]]])
+        
+        # cells that build the border of a patch
+        border_cells = x[hull,]
+        coordinates = as.matrix(border_cells[,coords])
+        coordinates <- rbind(coordinates, coordinates[1,])
+
+        return(st_polygon(list(coordinates)))
+    } else {
+        cur_coords <- as.matrix(cbind(x[[coords[1]]], x[[coords[2]]]))
+        hull <- data.frame(concaveman(cur_coords, concavity = 1))
+
+        return(st_polygon(list(as.matrix(hull))))
+    }
     
 }
