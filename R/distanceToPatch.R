4c46f14c5283570a40543f454f7752ada2efe5f1
SchulzDan <shooltz@gmx.de>
2022-07-05 07:42:33 +0000

    added distance to patch function

---
+++ R/distanceToPatch.R
@@ -0,0 +1,63 @@
+# function to calculate the distance to the border of patches of cells.
+# positive values are indicative of cells that are outside of an object and negative values are indicative of cells inside of an object.
+
+# @param object single cell object
+# @param var colData entry indicating which cells belong to a patch
+# @param name character specifying the name of the colData entry to safe the distances in.
+# @param coords character vector of length 2 specifying the names of the \code{colData} (for a \code{SingleCellExperiment} object) or the \code{spatialCoords} entries of the cells' x and y locations.
+# @param img_id single character indicating the \code{colData(object)} entry containing the unique image identifiers.
+# @param BPPARAM a \code{\link[BiocParallel]{BiocParallelParam-class}} object defining how to parallelize computations.
+distanceToPatch <- function(object,
+                                 var,
+                                 name,
+                                 coords,
+                                 img_id,
+                                 BPPARAM = SerialParam()){
+
+  cur_meta <- metadata(object)
+  metadata(object) <- list()
+
+  cur_intmeta <- int_metadata(object)
+
+  cur_out <- bplapply(
+    unique(colData(object)[[img_id]]),
+    function(x){
+
+
+      # get one image and all cells within the mask
+      cur_obj <- object[,as.character(colData(object)[[img_id]]) == x]
+
+      cur_obj[[name]] <- NA
+      if (sum(!is.na(colData(cur_obj)[[var]])) == 0) {
+        return(cur_obj)
+      }
+
+      # calculate the distances for all against all cells
+      distance <- dist(as.matrix(colData(cur_obj)[,coords]))
+      distance <- as.matrix(distance)
+
+      # select only those columns (cells) that are part of the patch
+      pos_dist <- distance[,!is.na(colData(cur_obj)[[var]])]
+      # for each row (cell) get the minimal distance to a cell of the patch (columns)
+       dist_to_patch <- rowMins(pos_dist)
+
+      # select only those columns (cells) that are NOT part of the patch
+      neg_dist <- distance[,is.na(colData(cur_obj)[[var]])]
+      # # for each row (cell) get the minimal distance to a cell NOT part of the patch (columns)
+      dist_from_patch <- rowMins(neg_dist)
+
+      # cells that had a 0 distance to the patch can be substitutes with the negative distances from the patch
+      dist_to_patch[dist_to_patch == 0] <- -dist_from_patch[dist_from_patch != 0]
+
+      cur_obj[[name]] <- dist_to_patch
+
+      return(cur_obj)
+    }, BPPARAM = BPPARAM)
+
+  cur_out <- do.call("cbind", cur_out)
+
+  metadata(cur_out) <- cur_meta
+  int_metadata(cur_out) <- cur_intmeta
+
+  return(cur_out)
+}
