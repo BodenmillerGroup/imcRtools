10c7cb862cfc7735994cf27c89f69b9fad6bdbd1
marlene1 <marlenechiara.lutz@uzh.ch>
2025-06-26 15:19:41 +0000

    updating param descriptions

--- R/plotInteractions.R
+++ R/plotInteractions.R
@@ -6,7 +6,7 @@
 #' arguments. The resulting plot can be further refined with `ggplot2` for node 
 #' styling and `ggraph` for edge-specific customization.
 #'
-#' @param out a data frame, usually output from \code{countInteractions} or 
+#' @param out a data frame, usually the output from \code{countInteractions} or 
 #' \code{testInteractions}, representing an edge list with columns \code{"group_by",
 #' "from_label" and "to_label"}. Additional columns may be included to specify 
 #' edge attributes (weight or color). 
@@ -31,16 +31,15 @@
 #' colored.
 #' @param node_label_color_fix single character specifying the color of all node
 #' labels.
-#' @param edge_color_by single character indicating the name of the newly added 
-#' column to \code{"out"} to represent edge colors. This column must assign a 
-#' unique value to each 'from_label'–'to_label' pair. Typically, these values encode 
-#' the direction of significantly interacting cell type pairs
+#' @param edge_color_by single character indicating the name of the column of \code{"out"} 
+#' used represent edge colors. This column is usually newly added by the user and must assign a 
+#' unique value to each 'from_label'–'to_label' pair. Typically, these values could encode 
+#' the direction of significantly interacting cell type pairs. 
 #' @param edge_color_fix single character specifying the color of all edges.
 #' @param edge_width_by single character indicating the name of the column of \code{"out"} 
 #' used to scale edge widths. The values in this column are averaged for each 
-#' 'from_label'–'to_label' pair. Typically, this is the 'ct' column from of \code{"out"}
-#' (if of \code{"out"} is the result of of \code{"testInteractions"}), or a newly 
-#' added column representing an interaction feature.
+#' 'from_label'–'to_label' pair. Typically, this could be the 'ct' column from 
+#' of \code{"out"} or a newly added column representing an interaction feature. 
 #' @param edge_width_fix single numeric specifying the width of all edges.
 #' @param draw_edges should edges be drawn between nodes? Defaults to TRUE.
 #' @param graph_layout single character of
