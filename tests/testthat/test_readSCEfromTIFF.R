2b42ea1e7df36d6812d2fad7425fff3feeac8db7
Ubuntu <ubuntu@spo-server.cloud.science-it.uzh.ch>
2025-09-30 13:51:22 +0000

    finalized tests

--- tests/testthat/test_readSCEfromTIFF.R
+++ tests/testthat/test_readSCEfromTIFF.R
@@ -15,32 +15,7 @@ test_that("readSCEfromTIFF function reads in correct objects.", {
                                            "Dy","Dy"), each = 500))
   expect_equal(cur_sce$sample_mass, rep(c("161", "162", 
                                           "163","164"), each = 500))
-  
   expect_equal(assayNames(cur_sce), "counts")
-  
-  img_df   <- read.csv(image_df_path, stringsAsFactors = FALSE)
-  img_df <- img_df[order(img_df$acquisition_description), ]
-  panel_df <- read.csv(panel_df_path, stringsAsFactors = FALSE)
-  tiff_files <- file.path(path, img_df$image)
-  
-  tiff_list <- lapply(seq_along(tiff_files), function(i) {
-    img <- readImage(tiff_files[i])
-    dims <- dim(img)
-    
-    mat <- matrix(img, nrow = dims[1]*dims[2], ncol = dims[3])
-    
-    rownames(mat) <- paste0(basename(img_df$acquisition_description[i]), "_", seq_len(nrow(mat)))
-    colnames(mat) <- panel_df$channel
-    
-    return(mat)
-  })
-  cur_counts <- do.call(rbind, tiff_list)
-  cur_counts <- t(cur_counts)
-
-  expect_equal(as.numeric(counts(cur_sce)[1, ]), as.numeric(cur_counts[1, ]))
-  expect_equal(as.numeric(counts(cur_sce)[2, ]), as.numeric(cur_counts[2, ]))
-  expect_equal(as.numeric(counts(cur_sce)[3, ]), as.numeric(cur_counts[3, ]))
-  expect_equal(as.numeric(counts(cur_sce)[4, ]), as.numeric(cur_counts[4, ]))
   expect_equal(dim(cur_sce), c(4, 2000))
   
   # Verbose output
